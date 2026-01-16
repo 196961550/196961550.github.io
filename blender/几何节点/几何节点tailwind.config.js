d388bc7c7d4ee88ec491df08d44d2ee2651764cf
sallystar <76920133+196961550@users.noreply.github.com>
2026-01-16 10:32:02 +0800

    Add files via upload

---
+++ blender/几何节点/几何节点tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4D90FE',
+                secondary: '#1F2937',
+                dark: '#111827',
+                'dark-card': '#374151',
+            },
+            fontFamily: {
+                inter: ['Inter', 'system-ui', 'sans-serif'],
+            },
+        }
+    }
+}
